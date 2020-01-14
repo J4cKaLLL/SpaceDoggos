c779911669b1c13be311b6ef0bc08b0538752d8c
Suchito Makaroto <J4cKaL.r@gmail.com>
2020-01-14 08:44:53 -0500

    SpaceDoggos

---
+++ contracts/SpaceDoggos.sol
@@ -0,0 +1,15 @@
+pragma solidity ^0.4.20;
+
+contract SpaceDoggos {
+
+    uint maxPlanetsPerSystem = 10;
+    uint minPlanetsPerSystem = 3;
+
+    uint planetCodeDigits = 7;
+    uint systemCodeDigits = 7;
+
+    // Write your solution below
+
+
+
+}
